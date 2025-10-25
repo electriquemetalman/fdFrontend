438e638c277cfbdcec575b47837c89a02d3bd0af
electriquemetalman <stivingtatga@gmail.com>
2025-10-25 15:32:23 -0400

    project structure

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react'
+import './Navbar.css'
+import {assets} from '../../assets/assets'
+
+const Navbar = () => {
+
+    const [menu, setMenu] = useState("home");
+
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" className='logo'/>
+      <ul className="navbar-menu">
+        <li onClick={()=>setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
+        <li onClick={()=>setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
+        <li onClick={()=>setMenu("mobile")} className={menu === "mobile" ? "active" : ""}>Mobile App</li>
+        <li onClick={()=>setMenu("contact")} className={menu === "contact" ? "active" : ""}>Contact</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" className="search"/>
+        <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="" className="basket"/>
+            <div className="dot"></div>
+        </div>
+        <button className="login-btn">Login</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
