3b40e163a3a10bbd8c8221a0fb69e57c133e50fc
electriquemetalman <stivingtatga@gmail.com>
2025-11-07 00:35:17 -0500

    finish payment getway, and build display orders of user

---
+++ src/pages/MyOrders/MyOrders.jsx
@@ -0,0 +1,56 @@
+import React, { useCallback, useContext, useEffect, useState } from 'react'
+import "./MyOrders.css"
+import { StoreContext } from '../../context/StoreContext'
+import axios from 'axios';
+import { assets } from '../../assets/assets';
+
+const MyOrders = () => {
+
+    const {url, token} = useContext(StoreContext);
+    const [data, setData] = useState([]);
+
+    const fetchOrders = useCallback ( async () => {
+        try {
+            const response = await axios.get(url+"/api/order/userorders", {headers:{token}});
+            setData(response.data.orders);
+        } catch (error) {
+            console.log(error)
+        }
+    },[token, url]) 
+
+    useEffect(() => {
+        if(token) {
+            fetchOrders();
+        }
+    },[fetchOrders, token])
+
+  return (
+    <div className='my-orders'>
+        <h2>MyOrders</h2>
+        <div className='container'>
+            {data.map((order, index) => {
+                return (
+                    <div key={index} className='my-orders-order'>
+                        <img src={assets.parcel_icon} alt="" />
+                        <p>
+                            {order.items.map((item, index) => {
+                                if (index === order.items.length-1){
+                                    return item.name + " x " + item.quantity
+                                } else {
+                                    return item.name + " x " + item.quantity + ", "
+                                }
+                            })}
+                        </p>
+                        <p>${order.amount}.00</p>
+                        <p>Items: {order.items.length}</p>
+                        <p><span>&#x25cf;</span> <b>{order.status}</b></p>
+                        <button>Track order</button>
+                    </div>
+                )
+            })}
+        </div> 
+    </div>
+  )
+}
+
+export default MyOrders
--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const PlaceOrder = () => {
 
@@ -50,6 +51,16 @@ const PlaceOrder = () => {
         alert("Error");
       }
     }
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      if(!token){
+        navigate('/cart');
+      } else if (getTotalCartAmount() === 0){
+        navigate('/cart');
+      }
+    },[token, navigate, getTotalCartAmount])
 
   return (
     <form onSubmit={placeOrder} className='place-order'>
