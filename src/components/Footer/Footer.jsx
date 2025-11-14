5707358306c361df9862486ab40f81b7aeb13b04
electriquemetalman <stivingtatga@gmail.com>
2025-11-14 02:59:55 -0500

    change Logo and fix footer logo display beug

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,8 +7,10 @@ const Footer = () => {
     <div className="footer" id='footer'>
       <div className="footer-content">
         <div className="footer-content-left">
-            <img src={assets.logo} alt='' />
-            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ametLorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ametLorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+            <div className="footer-logo">
+                <img src={assets.logo} alt='' />
+            </div>
+            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ametLorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ametLorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt='' />
                 <img src={assets.twitter_icon} alt='' />
