231d2789801165c2848f71e3156ed7bfd511d1de
electriquemetalman <stivingtatga@gmail.com>
2025-11-22 01:02:50 -0500

    add reel time notification systèm

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import StoreContextProvider from './context/StoreProvider.jsx';
+import NotificationProvider from './context/NotificationProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StoreContextProvider>
-      <App />
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
     </StoreContextProvider>
   </BrowserRouter>
 )
